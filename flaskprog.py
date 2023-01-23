cad57fd30336e67de9d67aaaf65c19b6908f918f
Jon Coe <jonmcoe@gmail.com>
2023-01-22 22:15:56 -0500

    fix argument processing for entered_values

--- flaskprog.py
+++ flaskprog.py
@@ -11,23 +11,17 @@ app = Flask(__name__)
 
 def prompt_values():
     if request.method == 'POST':
-        #entered_values_raw = request.form.getlist('entered_values_raw')
-        #entered_values = [int(val) for val in entered_values_raw.split(',') if int(val) in range(1, 7)]
         entered_values_raw = request.form['entered_values_raw']
-        print(entered_values_raw)
-        #temp_raw = entered_values_raw[0].split(', ')
-        temp_raw = entered_values_raw.split(', ')
-        print(temp_raw)
-        entered_values = []
-        for interval in temp_raw:
-            if int(interval) in range(1, 7):
-                entered_values.append(interval)
+        entered_values_as_int_list_unfiltered = [int(x) for x in entered_values_raw.split(',')]
+        entered_values = [x for x in entered_values_as_int_list_unfiltered if x > 0 and x < 7]
+        print('-------')
         print(entered_values)
+        print(type(entered_values))
+        print(type(entered_values[0]))
+        print('-----')
         binary_order = request.form['binary_order']
         show_binary = request.form['show_binary'] == 'True'
         initial_pitch = request.form['initial_pitch']
-        # Pass the values to main
-        #print(entered_values)
         run_standard_text_flow(entered_values, binary_order, show_binary, initial_pitch)
     else:
         return '''
